a1dbc7d27f1b0d094420a662625dd269989b00ff
devin <deivnobowen@gmail.com>
2018-03-15 15:46:06 -0600

    router is set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-
+import routes from './routes';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        test
+
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+          </nav>
+
+          <div className="body">
+          {routes}
+          </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    // <Provider store={createStore}>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    // </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
