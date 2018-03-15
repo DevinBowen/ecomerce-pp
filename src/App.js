8e3f5c8a04274775e1788def18eccdbe3bc4056b
devin <deivnobowen@gmail.com>
2018-03-15 16:39:45 -0600

    header css is started

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import routes from './routes';
 import { Link } from 'react-router-dom';
+import './css/app.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-          <nav>
+        <header className="header">
+        <div className="headLeft">
+          <div className="logo">logo</div>
+          <nav className="headNav">
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
           </nav>
-
-          <div className="body">
-          {routes}
           </div>
+          <div className="headRight">
+            <div className="directory">directory</div>
+            <div className="search">search</div>
+          </div>
+        </header>
+
+        <div className="body">
+          {routes}
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './css/reset.css'
 import registerServiceWorker from './registerServiceWorker';
 import { HashRouter } from 'react-router-dom';
 
