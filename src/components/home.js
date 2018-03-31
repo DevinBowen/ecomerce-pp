95365ec402330ec15b05c4816668f07f2b4fb973
devin <deivnobowen@gmail.com>
2018-03-31 13:05:24 -0600

    work dont on home page

--- src/components/home.js
+++ src/components/home.js
@@ -8,6 +8,7 @@ export default class Home extends Component {
     return (
       <div className="home">
         <div className="main_img">
+        <div>
           <div><img src={require("../images/Logo_Summit-Series-Horizontal-White.svg")} alt="summit logo" /></div>
           <div>ATHLETE
             <br/>
@@ -17,6 +18,11 @@ export default class Home extends Component {
               <br/>
               PROVEN.</div>
               <div><button>EXPLORE THE COLLECTION</button></div>
+          </div>
+          <div className="right_main">
+          <div className="right_one">Caro Ciavaldini: La Guerre Sainte</div>
+          <div className="right_two">Wadi Rum Preserve, Jordan</div>
+          </div>
         </div>
         <div className="three_boxes">
           <div className="box1">1</div>
