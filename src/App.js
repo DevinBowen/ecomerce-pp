c1bbda1637f6d0263e4c625f2361b637b5ce4dee
devin <deivnobowen@gmail.com>
2018-04-02 15:44:27 -0600

    home page is done except for the small details

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
             </div>
             <div className="search">
             <input type="text" placeholder="Search" />
-            <div className="enter_box">?</div>
+            <div className="enter_box"><img src={require("./images/search.png")} alt=""/></div>
             </div>
           </div>
         </header>
@@ -42,62 +42,90 @@ class App extends Component {
 
         <footer>
           <div className="site_map">
-            <div>SHOP
+          <div className="map_margin">
+            <div className="map_title">SHOP
               <div><ul>
-                  <li>Women's</li>
-                  <li>Men's</li>
-                  <li>Kids'</li>
-                  <li>Shoes</li>
-                  <li>Equipment</li>
-                  <li>Gift Cards</li>
+                  <a><li>Women's</li></a>
+                  <a><li>Men's</li></a>
+                  <a><li>Kids'</li></a>
+                  <a><li>Shoes</li></a>
+                  <a><li>Equipment</li></a>
+                  <a><li>Gift Cards</li></a>
                 </ul></div>
             </div>
-            <div>HELP
+            <div className="map_title">HELP
               <ul>
-                <li>FAQ?</li>
-                <li>VIPeak Rewards</li>
-                <li>Order Status</li>
-                <li>Sizing Charts</li>
-                <li>Warrenty Information</li>
-                <li>Return Policy</li>
-                <li>Contact Us</li>
+              <a><li>FAQ?</li></a>
+              <a><li>VIPeak Rewards</li></a>
+              <a><li>Order Status</li></a>
+              <a><li>Sizing Charts</li></a>
+              <a><li>Warrenty Information</li></a>
+              <a><li>Return Policy</li></a>
+              <a><li>Contact Us</li></a>
               </ul>
             </div>
-            <div>ABOUT TNF
+            <div className="map_title">ABOUT TNF
               <div><ul>
-                  <li>About Us</li>
-                  <li>Responsibility</li>
-                  <li>Technology & Innovation</li>
-                  <li>Expeditions</li>
-                  <li>Explore Fund Grants</li>
-                  <li>News</li>
-                  <li>Careers</li>
+              <a><li>About Us</li></a>
+              <a><li>Responsibility</li></a>
+              <a><li>Technology & Innovation</li></a>
+              <a><li>Expeditions</li></a>
+              <a><li>Explore Fund Grants</li></a>
+              <a><li>News</li></a>
+              <a><li>Careers</li></a>
                 </ul></div>
             </div>
-            <div>EVENTS
+            <div className="map_title">EVENTS
               <ul>
-                <li>Endurance Challenge</li>
-                <li>Speaker Series</li>
+              <a><li>Endurance Challenge</li></a>
+              <a><li>Speaker Series</li></a>
               </ul>
             </div>
-            <div>EXPLORE
+            <div className="map_title">EXPLORE
               <ul>
                 <li>The North Face App</li>
-                <li><img src="//images.thenorthface.com/is/image/TheNorthFaceBrand/app-store?fmt=png-alpha" /></li>
+                <a><li><img src="//images.thenorthface.com/is/image/TheNorthFaceBrand/app-store?fmt=png-alpha" /></li></a>
               </ul>
+            </div>
             </div>
           </div>
           <div className="connect">
           <div className="main_top">
-            <div>STORE LOCATOR</div>
-            <div>FOLLOW THE NORTH FACE</div>
-            <div>ENTER YOUR EMAIL</div>
+            <div>
+              <div className="map_title">STORE LOCATOR</div>
+              <div className="top_text">Explore a retail store or outlet near you.</div>
+              <button>FIND A STORE</button>
+            </div>
+            <div>
+              <div className="map_title">FOLLOW THE NORTH FACE</div>
+              <div className="top_text">
+                <img src="" alt="1"/>
+                <img src="" alt="2"/>
+                <img src="" alt="3"/>
+                <img src="" alt="4"/>
+                <img src="" alt="5"/>
+                <img src="" alt="6"/>
+              </div>
+              </div>
+            <div>
+              <div className="map_title">ENTER YOUR EMAIL</div>
+              <div className="top_text">Adventure is everywhere. Even in your inbox.</div>
+              <div className="top_input"><input type="text" placeholder="Email Address"/><div className="top_img">?<img src="" alt="arrow"/></div></div>
+              </div>
           </div>
           <div className="main_bottom">
             <div className="logo"></div>
+            <div className="bottom_text">
             <div>Ⓒ The North Face, A VF Company</div>
-            <div>CA Supply Chains Act      UK Slavery Act</div>
-            <div>Privacy Policy      Terms of Use</div>
+            <div className="bottom_row_one">
+            <div><a>CA Supply Chains Act &emsp;</a></div>
+            <div><a>UK Slavery Act</a></div>
+            </div>
+            <div className="bottom_row_two">
+            <div><a>Privacy Policy &emsp;</a></div>
+            <div><a>Terms of Use</a></div>
+            </div>
+            </div>
           </div>
           </div>
         </footer>
--- src/components/home.js
+++ src/components/home.js
@@ -7,6 +7,7 @@ export default class Home extends Component {
   render() {
     return (
       <div className="home">
+      <div className="shipping">FREE 3-DAY SHIPPING & FREE RETURNS</div>
         <div className="main_img">
         <div>
           <div><img src={require("../images/Logo_Summit-Series-Horizontal-White.svg")} alt="summit logo" /></div>
@@ -25,9 +26,31 @@ export default class Home extends Component {
           </div>
         </div>
         <div className="three_boxes">
-          <div className="box1">1</div>
-          <div className="box2">2</div>
-          <div className="box3">3</div>
+          <div className="box1"><img src={require("../images/home_middle.jpg")} alt=""/>
+          <div className="box_margin">
+          <div className="box_title">TAKE A HIKE</div>
+          <div className="box_text">Bag more peaks with technical gear that's built for the trails.</div>
+          <button>SHOP MEN'S</button>
+          <br/>
+          <button>SHOP WOMMEN'S</button>
+          </div>
+          </div>
+          <div className="box2"><img src={require("../images/home_left.jpg")} alt=""/>
+          <div className="box_margin">
+          <div className="box_title">APEX FLEX GTR® SEEK NO SHELTER</div>
+          <div className="box_text">Ultra-Soft. Impossibly Dry.</div>
+          <button>DISCOVER THE JACKET</button>
+          </div>
+          </div>
+          <div className="box3"><img src={require("../images/home_right.jpg")} alt=""/>
+          <div className="box_margin">
+          <div className="box_title">THE ULTRA FASTPACK COLLECTION</div>
+          <div className="box_text">Lighter, faster, more durable. The next generation of performance.</div>
+          <button>SHOP MEN'S</button>
+          <br/>
+          <button>SHOP WOMEN'S</button>
+          </div>
+          </div>
         </div>
       </div>
     );
