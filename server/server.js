8365f7bcfce7c1dbe60fc025612aeaff187f2788
devin <deivnobowen@gmail.com>
2018-04-06 23:49:15 -0600

    bugs

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
     ,massive = require('massive');
 require('dotenv').config();
 
+
 const app = express();
 app.use(bodyParser.json());
 app.use( cors() );
